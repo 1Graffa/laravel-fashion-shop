913aac4ea84b731663982622ce724c07bccf7f3c
Giuseppe <giusepperaffa1988@gmail.com>
2021-03-16 22:32:51 +0100

    header-nav

---
+++ laravel/resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('titolo','home page')
+
+@section('content')
+<h1>la mia home page</h1>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium, labore officiis et a maxime harum ad natus provident asperiores. Aspernatur a, earum rerum nam neque quae accusamus cum molestias sunt!</p>
+
+@endsection
